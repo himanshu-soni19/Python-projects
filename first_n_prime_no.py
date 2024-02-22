b1d868a21dc4eaf4360c84be142af06f73bf4a4b
Himanshu soni <himanshusoni19222@gmail.com>
2024-02-22 17:31:42 +0530

    Three new files added

---
+++ first_n_prime_no.py
@@ -0,0 +1,22 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def print_first_n_primes(n):
+    count = 0
+    num = 2
+    while count < n:
+        if is_prime(num):
+            print(num)
+            count += 1
+        num += 1
+
+
+n = int(input("Enter the value of n: "))
+print("The first", n, "prime numbers are:")
+print_first_n_primes(n)
---
+++ weight_unit_converter.py
@@ -0,0 +1,8 @@
+weight = int(input("Weight: "))
+unit = input("(K)g or (L)bs: ")
+if unit == "K":
+    converted = weight / 0.45
+    print("Weight in lbs: " + str(converted))
+else:
+    converted = weight * 0.45
+    print("Weight in Kgs: " + str(converted))
